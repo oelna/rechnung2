15f4714913ead2df11912384e0b22b9bd74c14d8
Arno Richter <mail@arnorichter.de>
2025-01-01 17:02:08 +0100

    Alpine rewrite, part 1

--- pages-main.js
+++ pages-main.js
@@ -1,60 +1,222 @@
 //import { createApp, reactive } from 'https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js'
 //import { vDraggable } from 'https://cdn.jsdelivr.net/npm/@neodrag/vue@2.2.0/+esm';
 
-import { default as Alpine } from 'https://cdn.jsdelivr.net/npm/alpinejs@3.14.7/+esm';
+import { default as Alpine } from 'alpine';
+import sort from 'alpinesort'
+import { Draggable } from 'neodrag';
 
 window.Alpine = Alpine;
+Alpine.plugin(sort);
 
 document.addEventListener('alpine:init', init);
 
 function init () {
-	console.log('x');
+	console.log('init');
+
+	// todo: put this somewhere that makes sense!
+	Alpine.store('global').cssRoot.style.setProperty('--page-width', '210mm');
+	Alpine.store('global').cssRoot.style.setProperty('--page-height', '297mm');
+
+	const toolbar = document.querySelector('.toolbar');
+
+	const options = {
+		threshold: { delay: 300 },
+		bounds: document.documentElement, // body element
+		disabled: false,
+		handle: '.handle',
+		onDragEnd: function (data) {
+			localStorage.setItem('toolbarPosition', JSON.stringify({ x: data.offsetX, y: data.offsetY }));
+		}
+	};
+
+	// restore UI toolbar position, if applicable
+	const storedPosition = localStorage.getItem('toolbarPosition');
+	if (storedPosition) {
+		options.position = JSON.parse(storedPosition);
+		console.log('restored position', options.position);
+	}
+
+	Alpine.store('global').ui.dragInstance = new Draggable(toolbar, options);
 }
 
-function Editor () {
+function Editor (props) {
 	return {
 		pages: [],
-		count: 2,
+		pageCount: 1,
 
-		init () {
-			console.log('Editor mounted!');
+		init (x) {
+			console.log('Editor mounted!', x, props);
 
-			// make first page
+			// todo: make first page
+			//this.addPage('page-1');
+			//this.activatePage(1);
+			/*
 			const newPage = Page({
 				id: 1
 			});
 			newPage.addElement('I am new');
 			this.pages.push(newPage);
+			*/
+			this.activatePage(1);
 		},
 		addPage (id) {
 			console.log('add Page!', id);
+			/*
 			const newPage = Page({
-				id: id
+				id: id,
+				dimensions: {
+					width: 210,
+					height: 297
+				}
 			});
-			this.pages.push(newPage);
+			this.pages.push(newPage); // todo: insert after currentPage!
+			*/
+			this.pageCount += 1;
+			console.log(this.pages);
+		},
+		deletePage (id) {
+			if (this.pages.length <= 1) {
+				window.alert('You cannot remove the last page!');
+				return;
+			}
+
+			if (window.confirm('Delete page ' + id + '?')) {
+				const p = document.querySelector('.page[data-order="'+id+'"]');
+				const pageID = p.getAttribute('data-page');
+
+				for (var i = 0; i < this.pages.length; i++) {
+					console.log(i, this.pages[i]);
+					if (this.pages[i].id == pageID) {
+						console.log('removed page reference');
+						this.pages.splice(i, 1);
+					}
+				}
+
+				p.remove();
+				// this.pageCount -= 1;
+				// this.activatePage(Math.max(1, this.pages.length));
+			}
+		},
+		activatePage (id) {
+			Alpine.store('global').currentPage = id;
+			console.log('activated page', Alpine.store('global').currentPage);
+		},
+		doPageSort (event) {
+
+			const self = this;
+
+			/*
+			document.querySelectorAll('.pages-navigator > div').forEach(function (ele, i) {
+				console.log(i, ele.id, ele.dataset.page);
+				const page = self.pages.find(function (element) {
+					console.log('x', element);
+					return element.id == ele.id;
+				});
+				console.log('found', self.pages);
+			});
+			*/
+
+			for (const [i, ele] of document.querySelectorAll('.pages-navigator > div').entries()) {
+				// console.log(i, ele.id, ele.dataset.page);
+				const page = self.pages.find(function (element) {
+					return element.id == ele.dataset.page;
+				});
+				page.order = i+1;
+				console.log('found', page.id);
+			}
+
+			return;
+
+			const { item, position } = event.detail;
+			console.log('notify page sort', item, position);
+
+			// todo: sort the actual pages
+			const pages = document.querySelectorAll('.doc .page');
+			const pagesOrdered = Array.from(pages).sort(function(a, b) {
+				const orderA = parseInt(window.getComputedStyle(a).getPropertyValue('order'));
+				const orderB = parseInt(window.getComputedStyle(b).getPropertyValue('order'));
+				console.log('sort', orderA, orderB);
+				return orderA - orderB;
+			});
+			console.log(pagesOrdered);
+			for (const p of pagesOrdered) {
+				const data = Alpine.evaluate(p, '$data');
+				// console.log('page at position', index, 'gets new position', data.order);
+				let newPosition;
+				if (data.order < item) {
+					newPosition = data.order-1;
+					console.log('lower page', newPosition);
+					// p.setOrder(position + 1);
+					// console.log('set order to', p.order);
+
+				} else if (data.order == item) {
+					newPosition = position+1;
+					console.log('found page to sort', newPosition);
+					// p.setOrder(position + 1);
+					// console.log('set order to', p.order);
+
+				} else {
+					newPosition = data.order;
+					console.log('higher page', newPosition);
+				}
+
+				p.dispatchEvent(new CustomEvent('orderchange', {
+					detail: {
+						position: newPosition
+					},
+					bubbles: false
+				}));
+			}
+
+
+		},
+		storeDocument () {
+
+		},
+		scrollTo (pageID) {
+			if (!pageID) return;
+			// console.log('p', pageID, this.pages[pageID-1].$el);
+			const pages = document.querySelectorAll('.doc .page');
+			const pageEle = pages[pageID-1];
+			pageEle.scrollIntoView({ behavior: 'smooth', block: "start" });
 		}
 	}
 }
-Alpine.data('editor', Editor);
+Alpine.data('editor', (props) => Editor(props));
 
 function Page (props) {
 	return {
 		id: null,
-		order: null,
+		order: props.order || null,
 		count: 0,
 		elements: [],
+		dimensions: {
+			width: 0,
+			height: 0
+		},
 
-		init (props) {
-			console.log('Page mounted!', props);
+		init () {
+			this.id = Alpine.store('global').uniqueString(4);
+
+			console.log('Page mounted! order:', this.order );
+			this.$data.pages.push(this);
+
+			// console.log('editor data', Alpine.evaluate(document.querySelector('.editor'), '$data'));
+			console.log(this.$data.pages);
 		},
 		addElement () {
 			console.log('add Element!');
+			// console.log($refs);
 
 			// make element
 			const newElement = Element({
 				// text: 'abc123'
 			});
 			this.elements.push(newElement);
+		},
+		setOrder (event) {
+			console.log('reorder page', this.id, event.detail.position);
+			this.order = event.detail.position;
 		},
 		deletePage (ele, index, $data) {
 			console.log('delete Page!', index, ele);
@@ -69,37 +231,139 @@ function Page (props) {
 		}
 	}
 }
-Alpine.data('page', Page);
+Alpine.data('page', (props) => Page(props));
+
+function PagesNavigator () {
+	return {
+
+		init () {
+			console.log('init navigator');
+		},
+		sortEnd (item, pos, $dispatch) {
+			// todo: reorder page numbers
+			$dispatch('sortpage', {
+				item: item,
+				position: pos
+			});
+
+			/*
+			window.addEventListener('sortpage', function (event) {
+				console.log('event', event.detail);
+			});
+
+			window.dispatchEvent(new CustomEvent('sortpage', {
+				detail: {
+					message: 'Hello World!',
+					item: item,
+					position: pos
+				},
+				bubbles: true
+			}));
+			*/
+		}
+	}
+}
+Alpine.data('pagesNavigator', PagesNavigator);
 
 function Element (props) {
 	return {
 		id: null,
+		parentPage: null,
 		position: {
 			x: 0,
 			y: 0
 		},
 		text: props?.text || "I'm a new element",
 
+		/*
+		// todo: getter and setter for mm values?
+		set price_USD (val) {
+			this.price_INR = val * 83;
+		},
+
+		get price_USD () {
+			return this.price_INR / 83;
+		}
+		*/
+
 		init () {
 			const id = Alpine.store('global').uniqueString(4);
 			this.id = id;
-			console.log('Element mounted!', this.id);
+			console.log('Element mounted!', this.$el, this.$refs.editor, this.id);
+
+			const options = {
+				// grid: [10, 10],
+				grid: [18.9, 18.9], // magic number: 5mm
+				threshold: { delay: 300 },
+				// bounds: 'parent',
+				disabled: false,
+				handle: '.handle',
+				// onDragStart: (data) => console.log('Dragging started', data),
+				onDrag: function (data) {
+					// console.log('Dragging', data.offsetX, data.offsetY, data.rootNode);
+					const frame = data.rootNode;
+					const frameRect = frame.getBoundingClientRect();
+					const elmTop = frameRect.top + window.scrollY;
+					const elmLeft = frameRect.left + window.scrollX;
+
+					const page = data.rootNode.closest('.page');
+					const pageRect = page.getBoundingClientRect();
+					const pageTop = pageRect.top + window.scrollY;
+					const pageLeft = pageRect.left + window.scrollX;
+
+					Alpine.store('global').currentFrame.position.x = Math.abs(pageLeft-elmLeft);
+					Alpine.store('global').currentFrame.position.y = Math.abs(pageTop-elmTop);
+
+					// console.log('drag', Alpine.store('global').currentFrame.position);
+					/*
+					this.position.x = pageLeft-elmLeft;
+					this.position.y = pageTop-elmTop;
+
+					console.log(this.position);
+					*/
+				},
+				onDragEnd: function (data) {
+					console.log('Dragging stopped', data);
+					Alpine.store('global').currentFrame = null;
+				}
+			};
+
+			const dragInstance = new Draggable(this.$el, options);
+		},
+		activate (ele) {
+			Alpine.store('global').currentFrame = ele;
+			console.log('activate', ele);
+		},
+		getXmm () {
+			return Alpine.store('global').pxmm(this.position.x).toFixed(2);
+		},
+		getYmm () {
+			return Alpine.store('global').pxmm(this.position.y).toFixed(2);
 		}
 	}
 }
-Alpine.data('element', Element);
+Alpine.data('element', (props) => Element(props));
 
 Alpine.store('global', {
+	screenDPI: 96, // todo: magic
 	pageIDs: 1,
-	ui: [],
+	ui: {
+		dragInstance: null
+	},
 	documents: [],
 	currentDocument: null,
+	currentFrame: null,
+	currentPage: null,
+	cssRoot: document.querySelector(':root'),
 
-	uniqueString(numBytes) {
+	uniqueString (numBytes) {
 		const bytes = crypto.getRandomValues(new Uint8Array(numBytes));
 		const array = Array.from(bytes);
 		const hexPairs = array.map(b => b.toString(16).padStart(2, '0'));
 		return hexPairs.join('');
+	},
+	pxmm (px) {
+		return (px * 25.4) / this.screenDPI;
 	}
 });
 
